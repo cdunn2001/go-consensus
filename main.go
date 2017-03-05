914d2356fbf49b05421b0e1549078bf469ec9035
Christopher Dunn <cdunn2001@gmail.com>
2017-03-05 14:50:07 -0600

    get_longest_reads

--- main.go
+++ main.go
@@ -4,7 +4,64 @@ package main
 import "fmt"
 import "github.com/jessevdk/go-flags"
 import "os"
+import "sort"
 
+func Use(vals ...interface{}) {
+	for _, val := range vals {
+		_ = val
+	}
+}
+
+type NtSlice []byte
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+func get_longest_reads(seqs []NtSlice, max_n_read, max_cov_aln int) []NtSlice {
+	longest_n_reads := max_n_read
+	if max_cov_aln > 0 {
+		longest_n_reads = 1
+		seed_len := len(seqs[0])
+		read_cov := 0
+		for _, seq := range seqs[1:] {
+			if read_cov/seed_len > max_cov_aln {
+				break
+			}
+			longest_n_reads += 1
+			read_cov += len(seq)
+		}
+		longest_n_reads = min(longest_n_reads, max_n_read)
+	}
+	return seqs[:longest_n_reads]
+}
+
+type ByLongestNtSlice []NtSlice
+
+func (slice ByLongestNtSlice) Len() int {
+	return len(slice)
+}
+func (slice ByLongestNtSlice) Less(i, j int) bool {
+	return len(slice[i]) > len(slice[j])
+}
+func (slice ByLongestNtSlice) Swap(i, j int) {
+	slice[i], slice[j] = slice[j], slice[i]
+}
+func get_longest_sorted_reads(seqs []NtSlice, max_n_read, max_cov_aln int) []NtSlice {
+	n := len(seqs)
+	my_seqs := make([]NtSlice, n)
+	copy(my_seqs, seqs)
+	sort.Sort(ByLongestNtSlice(my_seqs[1:n]))
+	return get_longest_reads(my_seqs, max_n_read, max_cov_aln)
+}
+func get_consensus_with_trim(c_input int) (string, int) {
+	return "hi", 0
+}
+func get_consensus_without_trim(c_input int) (string, int) {
+	return "bye", 1
+}
 func main() {
 	println("hello!")
 	var opts struct {
@@ -35,5 +92,33 @@ func main() {
 		//panic(err)
 		os.Exit(2)
 	}
-	fmt.Println(args, err)
+	fmt.Println(opts, args, err)
+	type GetConsensusFunc func(int) (string, int)
+	var get_consensus GetConsensusFunc
+	if opts.Trim {
+		get_consensus = get_consensus_with_trim
+	} else {
+		get_consensus = get_consensus_without_trim
+	}
+	K := 8
+	Use(get_consensus, K)
+	//config = args.min_cov, K, args.max_n_read, args.min_idt, args.edge_tolerance, args.trim_size, args.min_cov_aln, args.max_cov_aln
+	type SeqData struct {
+		K                                              int
+		min_cov, max_n_read, edge_tolerance, trim_size int
+		min_cov_aln, max_cov_aln                       int
+		min_idt                                        float32
+	}
+	config := SeqData{
+		K:              K,
+		min_cov:        opts.Min_cov,
+		max_n_read:     opts.Max_n_read,
+		min_idt:        opts.Min_idt,
+		edge_tolerance: opts.Edge_tolerance,
+		trim_size:      opts.Trim_size,
+		min_cov_aln:    opts.Min_cov_aln,
+		max_cov_aln:    opts.Max_cov_aln,
+	}
+	fmt.Println(config)
+	Use(config)
 }
